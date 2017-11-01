97bb41c1a647d0b4be1f6c9de672d3ed4743acf0
Kenny Cao <kencao@cs.stonybrook.edu>
2017-11-01 16:14:53 -0400

    (echo Hello > output.txt), (cat < input.txt), (grep hello < input.txt) all work now

--- hw4/src/main.c
+++ hw4/src/main.c
@@ -7,6 +7,7 @@
 #include <readline/readline.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #include "sfish.h"
 #include "debug.h"
@@ -40,39 +41,73 @@ int main(int argc, char *argv[], char* envp[]) {
             strcat(prompt, " :: kencao >> ");
         }
         input = readline(prompt);
+        if (input == NULL) {
+            free(prompt);
+            free(input);
+            continue;
+        }
 
-        write(1, "\e[s", strlen("\e[s"));
-        write(1, "\e[20;10H", strlen("\e[20;10H"));
-        write(1, "SomeText", strlen("SomeText"));
-        write(1, "\e[u", strlen("\e[u"));
+        parseLine(input, argv);
+        if (getenv("PPATH") == NULL) {
+            setenv("PPATH", get_current_dir_name(), 1);
+        }
+        //write(1, "\e[s", strlen("\e[s"));
+        //write(1, "\e[20;10H", strlen("\e[20;10H"));
+        //write(1, "SomeText", strlen("SomeText"));
+        //write(1, "\e[u", strlen("\e[u"));
 
         // If EOF is read (aka ^D) readline returns NULL
-        if (strcmp(input, "help") == 0) HELP();
-        else if (strcmp(input, "exit") == 0) break;
-        else if (strcmp(input, "pwd") == 0) printf("%s\n", get_current_dir_name());
-        else if (input[0] == 'c' && input[1] == 'd') {
-            if (*(input + 3) == 0 || *(input + 3) == 32 || strlen(input) == 2) {
-                if (getenv("PPATH") != get_current_dir_name()) setenv("PPATH", get_current_dir_name(), 1);
+        if (strcmp(argv[0], "help") == 0) HELP();
+        else if (strcmp(argv[0], "exit") == 0) break;
+        else if (strcmp(argv[0], "pwd") == 0) printf("%s\n", get_current_dir_name());
+        else if (strcmp(argv[0], "cd") == 0) {
+            if (argv[1] == NULL || strcmp(argv[1], " ") == 0) {
+                //if (getenv("PPATH") != get_current_dir_name()) setenv("PPATH", get_current_dir_name(), 1);
                 chdir(getenv("HOME"));
             }
-            else if (*(input + 3) == '-') {
+            else if (strcmp(argv[1], "-") == 0) {
                 chdir(getenv("PPATH"));
             }
             else {
                 if (getenv("PPATH") != get_current_dir_name()) setenv("PPATH", get_current_dir_name(), 1);
-                chdir(input + 3);
+                chdir(argv[1]);
             }
         }
         else {
-            parseLine(input, argv);
             pid_t pid;
             int childStatus;
-            if ((pid = fork()) == 0) execvp(argv[0], argv);
-            waitpid(pid, &childStatus, 0);
-        }
+            //stdout = fmemopen(argv[1], strlen(argv[1]) + 1, "r+");
+            if ((pid = fork()) == 0) {
+                if (strcmp(argv[0], "ls") == 0) {
 
-        if(input == NULL) {
-            continue;
+                }
+                else if (strcmp(argv[0], "echo") == 0) {
+                    if (strcmp(argv[2], ">") == 0) {
+                        int fd;
+                        fd = open(argv[3], O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+                        dup2(fd, 1);
+                        argv[2] = 0;
+                        argv[3] = 0;
+                    }
+                }
+                else if (strcmp(argv[0], "cat") == 0) {
+                    if (strcmp(argv[1], "<") == 0) {
+                        int fd;
+                        fd = open(argv[2], O_RDONLY);
+                        argv[1] = 0;
+                        dup2(fd, 0);
+                    }
+                }
+                else if (strcmp(argv[0], "grep") == 0) {
+                    if (strcmp(argv[2], "<") == 0) {
+                        int fd = open(argv[3], O_RDONLY);
+                        argv[2] = 0;
+                        dup2(fd, 0);
+                    }
+                }
+                execvp(argv[0], argv);
+            }
+            waitpid(pid, &childStatus, 0);
         }
 
 
@@ -85,10 +120,7 @@ int main(int argc, char *argv[], char* envp[]) {
         // Readline mallocs the space for input. You must free it.
         free(prompt);
         free(input);
-
     } while(!exited);
-
-    printf("%s\n", getenv("PATH"));
 
     debug("%s", "user entered 'exit'");
 
@@ -103,14 +135,28 @@ int parseLine(char *buf, char **argv) {
     buf[strlen(buf)] = ' ';
     while (*buf && (*buf == ' '))
         buf++;
-
     argc = 0;
+    /*delim = strchr(buf, ' ');
+    argv[argc++] = buf;
+    *delim = '\0';
+    buf = delim + 1;
+    while (*buf && (*buf == ' ')) buf++;*/
     while ((delim = strchr(buf, ' '))) {
-        argv[argc++] = buf;
-        *delim = '\0';
-        buf = delim + 1;
-        while (*buf && (*buf == ' '))
-            buf++;
+        if (buf[0] != '"') {
+            argv[argc++] = buf;
+            *delim = '\0';
+            buf = delim + 1;
+            while (*buf && (*buf == ' '))
+                buf++;
+        }
+        else {
+            delim = strchr(buf + 1, '"');
+            argv[argc++] = buf + 1;
+            *delim = '\0';
+            buf = delim + 1;
+            while (*buf && (*buf == ' '))
+                buf++;
+        }
     }
     argv[argc] = NULL;
 
