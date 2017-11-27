d5e0fb47482eff047234335509dc53ef44b2d58f
Kenny Cao <kencao@cs.stonybrook.edu>
2017-11-27 13:29:49 -0500

    Queue should be properly implemented

--- hw5/src/cream.c
+++ hw5/src/cream.c
@@ -1,6 +1,24 @@
 #include "cream.h"
 #include "utils.h"
+#include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    queue_t *newQ;
+    newQ = create_queue();
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int x = 10;
+    for (int i = 0; i < x; i++) {
+        enqueue(newQ, &array[i]);
+    }
+    for (int i = 0; i < x; i++) {
+        printf("%d\n", *(int*)newQ->front->item);
+        dequeue(newQ);
+    }
+    x = 100;
+    enqueue(newQ, &x);
+    printf("%d\n", *(int*)newQ->front->item);
+    dequeue(newQ);
     exit(0);
 }
--- hw5/src/queue.c
+++ hw5/src/queue.c
@@ -1,17 +1,55 @@
 #include "queue.h"
+#include <errno.h>
+#include <stdio.h>
 
 queue_t *create_queue(void) {
-    return NULL;
+    queue_t *newQueue;
+    newQueue = calloc(1, sizeof(queue_t));
+    if (newQueue == NULL) return NULL;
+    if (pthread_mutex_init(&(newQueue->lock), NULL) != 0) return NULL;
+    sem_init(&(newQueue->items), 0, 0);
+    newQueue->invalid = false;
+    return newQueue;
 }
 
 bool invalidate_queue(queue_t *self, item_destructor_f destroy_function) {
+    self->invalid = true;
     return false;
 }
 
 bool enqueue(queue_t *self, void *item) {
-    return false;
+    pthread_mutex_lock(&(self->lock));
+    if (self == NULL || self->invalid == true) {
+        errno = EINVAL;
+        return false;
+    }
+    queue_node_t *newNode = calloc(1, sizeof(queue_node_t));
+    if (newNode == NULL) return false;
+    newNode->item = item;
+    int x = 0;
+    sem_getvalue(&(self->items), &x);
+    if (x == 0) {
+        self->front = newNode;
+        self->rear = newNode;
+    } else {
+        self->rear->next = newNode;
+        self->rear = newNode;
+    }
+    pthread_mutex_unlock(&(self->lock));
+    sem_post(&(self->items));
+    return true;
 }
 
 void *dequeue(queue_t *self) {
+    pthread_mutex_lock(&(self->lock));
+    if (self == NULL) {
+        errno = EINVAL;
+        return NULL;
+    }
+    sem_wait(&(self->items));
+    queue_node_t *remove = self->front;
+    self->front = self->front->next;
+    free(remove);
+    pthread_mutex_unlock(&(self->lock));
     return NULL;
 }
