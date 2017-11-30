c922ebb6d1e61cb653b19979245fa26924f3cd6b
Kenny Cao <kencao@cs.stonybrook.edu>
2017-11-30 17:22:00 -0500

    Data structures are complete

--- hw5/src/cream.c
+++ hw5/src/cream.c
@@ -4,46 +4,130 @@
 #include "hashmap.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define NUM_THREADS 100
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
-//#define MAP_KEY(kbase, klen) (map_key_t) {.key_base = kbase, .key_len = klen}
-//#define MAP_VAL(vbase, vlen) (map_val_t) {.val_base = vbase, .val_len = vlen}
+queue_t *globalQueue;
+hashmap_t *globalMap;
 
-hashmap_t *global_map;
+pthread_mutex_t lock;
 
-uint32_t someHash(map_key_t map_key) {
-    const uint8_t *key = map_key.key_base;
-    size_t length = map_key.key_len;
-    size_t i = 0;
-    uint32_t hash = 0;
+int openListenFD(int portNumber, int maxEntries);
+void *connectionHandler(void *arg);
 
-    while (i != length) {
-        hash += key[i++];
-        hash += hash << 10;
-        hash ^= hash >> 6;
-    }
-
-    hash += hash << 3;
-    hash ^= hash >> 11;
-    hash += hash << 15;
-    return hash;
+void error(char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
 
-void freeMe(map_key_t key, map_val_t val) {
-    free(key.key_base);
-    free(val.val_base);
+void destroyHashMap() {
+}
+
+void destroyQueue() {
 }
 
 int main(int argc, char *argv[]) {
-    global_map = create_map(10, someHash, freeMe);
-    int x = 5;
-    put(global_map, MAP_KEY(&x, sizeof(int)), MAP_VAL(&x, sizeof(int)), false);
-    int y = 15;
-    put(global_map, MAP_KEY(&y, sizeof(int)), MAP_VAL(&y, sizeof(int)), false);
-    int z = 16;
-    put(global_map, MAP_KEY(&z, sizeof(int)), MAP_VAL(&z, sizeof(int)), false);
-    delete(global_map, MAP_KEY(&y, sizeof(int)));
-    map_val_t val = get(global_map, MAP_KEY(&z, sizeof(int)));
-    printf("%d, %lu\n", *(int*)val.val_base, val.val_len);
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0) {
+            printf("./cream [-h] NUM_WORKERS PORT_NUMBER MAX_ENTRIES\n\
+-h                 Displays this help menu and returns EXIT_SUCCESS.\n\
+NUM_WORKERS        The number of worker threads used to service requests.\n\
+PORT_NUMBER        Port number to listen on for incoming connections.\n\
+MAX_ENTRIES        The maximum number of entries that can be stored in `cream`'s underlying data store.\n");
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    //Set arguments to given variables
+    struct sockaddr_in clientAddr;
+    int connectionFD, clientLength;
+
+    int numWorkers = atoi(argv[1]);
+    int portNumber = atoi(argv[2]);
+    int maxEntries = atoi(argv[3]);
+
+    globalQueue = create_queue(); //Create a new queue
+    globalMap = create_map(maxEntries, jenkins_one_at_a_time_hash, destroyHashMap); //Create a new hashmap
+
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_t threadIDs[numWorkers]; //Spawn NUM_WORKERS amount of threads
+    for (int i = 0; i < numWorkers; i++) {
+        pthread_create(&threadIDs[i], NULL, connectionHandler, &connectionFD);
+    }
+
+    int listenFD = openListenFD(portNumber, maxEntries); //Open socket, bind to specified PORT_NUMBER and infinitely listen
+    clientLength = sizeof(clientAddr);
+
+    //int i = 0;
+
+    //request_header_t requestHeader;
+
+    //Accept a client's connection
+    if ((connectionFD = accept(listenFD, (struct sockaddr*) &clientAddr, (socklen_t*) &clientLength)) < 0)
+        error("ERROR on accept");
+
+    enqueue(globalQueue, &connectionFD);
+
+//    read(connectionFD, &requestHeader, sizeof(requestHeader));
+
+//    printf("%d\n", requestHeader.request_code);
+
+    //printf("%d\n", *(int*)globalQueue->front->item);
+
+
+
+    /*while (connectionFD = accept(listenFD, (struct sockaddr*) &clientAddr, (socklen_t*) &clientLength)) {
+        if (pthread_create(&threadIDs[i], NULL, connectionHandler, connectionFD) < 0)
+            error("ERROR on creating thread");
+
+        pthread_join(threadIDs[i], NULL);
+    }
+
+    if (connectionFD < 0)
+        error("ERROR on accept");
+    */
+
+
+
     exit(0);
+}
+
+void *connectionHandler(void *arg) {
+    //pthread_mutex_lock(&lock);
+    //int clientFD = (int*) arg;
+    //pthread_mutex_unlock(&lock);
+    int clientFD = *(int*) arg;
+    request_header_t requestHeader;
+    read(clientFD, &requestHeader, sizeof(requestHeader));
+    printf("%d\n", requestHeader.request_code);
+    return NULL;
+}
+
+int openListenFD(int portNumber, int maxEntries) {
+    struct sockaddr_in servAddr;
+
+    int sockFD, optval = 1;
+    if ((sockFD = socket(AF_INET, SOCK_STREAM, 0)) < 0) //Open a socket
+        error("ERROR on opening socket");
+
+    if (setsockopt(sockFD, SOL_SOCKET, SO_REUSEADDR, (const void*) &optval, sizeof(int)) < 0)
+        error("ERROR on setsockopt");
+
+    bzero((char*)&servAddr, sizeof(servAddr)); //Set to 0
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons((unsigned short)portNumber);
+
+    //Bind socket to PORT_NUMBER
+    if (bind(sockFD, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0)
+        error("ERROR on binding");
+    //Infinitely listen?
+    listen(sockFD, maxEntries);
+
+    return sockFD;
 }
